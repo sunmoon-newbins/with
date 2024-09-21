61a598ba8a789990b82ce1ad1681f8cb1bfbdd71
seohansoo <hansoo4799@gmail.com>
2024-09-21 23:03:50 +0900

    :tada: Init: create springboot project

---
+++ with_backend/src/main/java/com/newbins/with_backend/WithBackendApplication.java
@@ -0,0 +1,13 @@
+package com.newbins.with_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithBackendApplication.class, args);
+	}
+
+}
---
+++ with_backend/src/test/java/com/newbins/with_backend/WithBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newbins.with_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
