f839c56160b7cbf4ad03314acaaf67326c30ae54
박준엽 <86219320+parkjunyeap@users.noreply.github.com>
2024-09-23 18:27:42 +0900

    :tada: :sparkles:  Feat: loginScreen, StartScreen, Stacknavigation (#2)
    기본적인 틀 구성과
    로그인, 시작화면 생성
    
    다음 할일 바텀탭, 회원가입

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+function HomeScreen() {
+  return (
+    <View>
+      <Text>헬로</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
