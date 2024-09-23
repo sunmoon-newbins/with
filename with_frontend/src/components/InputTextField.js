a17a20c921fe3072416194d2506dc977c5beca00
parkjunyeap <bab85503@gmail.com>
2024-09-23 18:51:42 +0900

    :sparkles: Feat: loginScreen, StartScreen, Stacknavigation

---
+++ with_frontend/src/components/InputTextField.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+const InputTextField = ({
+  label,
+  placeholder,
+  value,
+  onChangeText,
+  secureTextEntry,
+  accessibilityLabel,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        placeholderTextColor="#9094B8"
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+        accessibilityLabel={accessibilityLabel}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+  },
+  label: {
+    color: "#0B1527",
+    fontSize: 15,
+    fontFamily: "Inter, sans-serif",
+    marginBottom: 12,
+  },
+  input: {
+    fontFamily: "Inter, sans-serif",
+    fontSize: 16,
+    backgroundColor: "#F4F8FB",
+    borderRadius: 5,
+    padding: 10,
+    color: "#0B1527",
+  },
+});
+
+export default InputTextField;
