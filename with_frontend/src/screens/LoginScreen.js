96731fc2a878f4135ff6bbb414dbe24ea1666ae6
박준엽 <86219320+parkjunyeap@users.noreply.github.com>
2024-09-23 18:53:38 +0900

    :sparkles: Feat: loginScreen, StartScreen, Stacknavigation (#3)

---
+++ with_frontend/src/screens/LoginScreen.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  ScrollView,
+} from "react-native";
+import WelcomeMessage from "../components/LoginScreen/WelcomeMessage";
+import LoginForm from "../components/LoginScreen/LoginForm";
+import LanguageButtons from "../components/LoginScreen/LanguageButtons";
+import Style from "../configs/Style.json";
+
+const LoginScreen = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.headerContainer}>
+        <ImageBackground
+          resizeMode="contain"
+          source={require("../../assets/back.png")}
+          style={styles.headerImage}
+        >
+          <Text style={styles.headerText}>로그인</Text>
+        </ImageBackground>
+      </View>
+      <View style={styles.contentContainer}>
+        <WelcomeMessage />
+        <LoginForm />
+        <LanguageButtons />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: "#5775CD",
+  },
+  headerContainer: {
+    alignItems: "center",
+    paddingTop: 37,
+    paddingBottom: 5,
+  },
+  headerImage: {
+    aspectRatio: 1.91, // 너비가 높이의 1.91배가 됨
+    width: "100%",
+    maxWidth: 277,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerText: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "700",
+    fontFamily: "Inter, sans-serif",
+    letterSpacing: 0.34,
+  },
+  contentContainer: {
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+  },
+});
+
+export default LoginScreen;
