804c2f383945447717b264dd01fd391724ede458
parkjunyeap <bab85503@gmail.com>
2024-09-24 13:32:37 +0900

    :sparkles: Design BottomTaps = Figma
    피그마대로 디자인 성공, 적응형 이렇게 쓰면 일일이 해야하는데 어떻게 해야할지 고민 .
    
    NEXT) 게시글 메인화면 , zustand 사용법 익히면서 모두 전역관리

--- with_frontend/src/navigations/BottomTabNavigator.js
+++ with_frontend/src/navigations/BottomTabNavigator.js
@@ -1,106 +1,29 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image, View, Text } from "react-native"; // Image와 View, Text 컴포넌트 임포트
-
-import Style from "../configs/Style.json"; // 파일명 from 풀경로
-
-// 화면 컴포넌트 임포트
 import HomeNavigator from "../navigations/BottomStackNavigations/HomeNavigator";
 import HeartNavigator from "../navigations/BottomStackNavigations/HeartNavigator";
 import RouteCreateNavigator from "../navigations/BottomStackNavigations/RouteCreateNavigator";
 import ChatNavigator from "../navigations/BottomStackNavigations/ChatNavigator";
 import MyInfoNavigator from "../navigations/BottomStackNavigations/MyInfoNavigator";
 
-const BTab = createBottomTabNavigator();
+import CustomTabBar from "../components/common/CustomTabBar"; // CustomTabBar 컴포넌트 임포트
 
-const TabBarIcon = ({ source, size, color }) => {
-  return (
-    <Image
-      source={source}
-      style={{ width: size, height: size, tintColor: color }}
-      resizeMode="contain"
-    />
-  );
-};
+const BTab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
     <BTab.Navigator
       initialRouteName="HomeNavigator"
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, size, color }) => {
-          let iconSource;
-
-          switch (route.name) {
-            case "Home":
-              iconSource = require("../../assets/BottomHome.png");
-              size = 40;
-              break;
-            case "Heart":
-              iconSource = require("../../assets/BottomHeart.png");
-              size = 40;
-              break;
-            case "Add":
-              iconSource = require("../../assets/BottomAdd.png");
-              size = 45;
-              break;
-            case "Chat":
-              iconSource = require("../../assets/BottomChat.png");
-              size = 40;
-              break;
-            case "User":
-              iconSource = require("../../assets/BottomUser.png");
-              size = 40;
-              break;
-          }
-
-          return (
-            <TabBarIcon
-              source={iconSource}
-              size={size}
-              color={focused ? "#5775CD" : "#8F95B7"}
-            />
-          );
-        },
-        tabBarShowLabel: false, // 라벨 숨기기
-        tabBarStyle: {
-          backgroundColor: "#ffffff",
-          borderTopWidth: 0,
-          elevation: 0,
-          height: 55, // 탭바 크기 근데 반응형으로 다른 스마트폰이면 어떡하죠??
-        },
-        tabBarItemStyle: ({ focused }) => ({
-          backgroundColor: focused ? "#F4F8FB" : "#ffffff",
-          justifyContent: "center",
-          alignItems: "center",
-        }),
-      })}
+      tabBar={(props) => <CustomTabBar {...props} />} // 커스텀 탭바 사용
+      screenOptions={{
+        headerShown: false,
+      }}
     >
-      <BTab.Screen
-        name="Home"
-        component={HomeNavigator}
-        options={{ headerShown: false }}
-      />
-      <BTab.Screen
-        name="Heart"
-        component={HeartNavigator}
-        options={{ headerShown: false }}
-      />
-      <BTab.Screen
-        name="Add"
-        component={RouteCreateNavigator}
-        options={{ headerShown: false }}
-      />
-      <BTab.Screen
-        name="Chat"
-        component={ChatNavigator}
-        options={{ headerShown: false }}
-      />
-      <BTab.Screen
-        name="User"
-        component={MyInfoNavigator}
-        options={{ headerShown: false }}
-      />
+      <BTab.Screen name="Home" component={HomeNavigator} />
+      <BTab.Screen name="Heart" component={HeartNavigator} />
+      <BTab.Screen name="Add" component={RouteCreateNavigator} />
+      <BTab.Screen name="Chat" component={ChatNavigator} />
+      <BTab.Screen name="User" component={MyInfoNavigator} />
     </BTab.Navigator>
   );
 };
