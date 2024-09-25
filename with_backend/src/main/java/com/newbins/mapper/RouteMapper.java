26a9701e16c49f1ca7fc43d8e3a3e5cd8b9bd378
ramgthunder <ramgthunder12@gmail.com>
2024-09-25 16:15:43 +0900

    :sparkles:Feat:createRoute feature

--- with_backend/src/main/java/com/newbins/mapper/RouteMapper.java
+++ with_backend/src/main/java/com/newbins/mapper/RouteMapper.java
@@ -1,7 +1,13 @@
 package com.newbins.mapper;
 
+import com.newbins.dto.Route;
 import org.apache.ibatis.annotations.Mapper;
+
 
 @Mapper
 public interface RouteMapper {
+    void createRoute(Route route);
+
+
 }
+
