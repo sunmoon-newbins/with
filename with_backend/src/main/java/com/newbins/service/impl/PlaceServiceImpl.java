af58bf45dcbd9a14f3d3aa3721c3121450f87710
ramgthunder <ramgthunder12@gmail.com>
2024-09-29 18:58:14 +0900

    create searchPlaces by placeName

--- with_backend/src/main/java/com/newbins/service/impl/PlaceServiceImpl.java
+++ with_backend/src/main/java/com/newbins/service/impl/PlaceServiceImpl.java
@@ -1,10 +1,38 @@
 package com.newbins.service.impl;
 
+import com.newbins.dto.Place;
+import com.newbins.dto.Route;
+import com.newbins.entity.PlaceEntity;
+import com.newbins.entity.RouteEntity;
+import com.newbins.mapper.PlaceMapper;
 import com.newbins.service.PlaceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Slf4j
 public class PlaceServiceImpl implements PlaceService {
+    private final PlaceMapper placeMapper;
+
+    public PlaceServiceImpl(PlaceMapper placeMapper) {
+        this.placeMapper = placeMapper;
+    }
+
+    @Override
+    public List<Place> searchPlaces(String placeName) {
+        log.info("[searchPlaces] : search = {}", placeName);
+
+        List<PlaceEntity> placeEntities = placeMapper.getPlaces(placeName);
+        List<Place> places = new ArrayList<>();
+        for (PlaceEntity entity : placeEntities) {
+            Place place = new Place();
+            places.add(place.toDTO(entity));
+        }
+
+        return places;
+    }
+
 }
